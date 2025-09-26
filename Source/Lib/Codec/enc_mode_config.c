7cb23db4e6b2024d8c8563ad4c573ab2e075fb21
Julio Barba <juliobbv@gmail.com>
2025-09-26 15:47:14 -0400

    Implement extended CRF range (63.25 to 70)

--- Source/Lib/Codec/enc_mode_config.c
+++ Source/Lib/Codec/enc_mode_config.c
@@ -7893,6 +7893,13 @@ set lpd0_level
             }
         }
     }
+
+    // Extended CRF range (63.25 - 70), increase lambda weight toward further bit saving
+    // Max lambda weight increase: 28 * 28 = 784
+    if (scs->static_config.qp == MAX_QP_VALUE && scs->static_config.extended_crf_qindex_offset) {
+        pcs->lambda_weight += scs->static_config.extended_crf_qindex_offset * 28;
+    }
+
     uint8_t dlf_level = 0;
     if (pcs->scs->static_config.enable_dlf_flag && frm_hdr->allow_intrabc == 0) {
         EncMode dlf_enc_mode = enc_mode;
