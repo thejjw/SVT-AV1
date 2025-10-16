7c1bb8d80fad460eee0671629188fc87010c0aec
Li Zhang <li.zhang2@arm.com>
2025-10-16 17:32:47 +0200

    Use Neon I8MM 8-tap path for 6-tap filters in av1_convolve_y_sr
    The 8-tap Neon I8MM path is faster than the Armv8.0 Neon 6-tap path that
    is currently used.
    
    This is a port from libaom.

--- Source/Lib/ASM_NEON_I8MM/convolve_neon_i8mm.c
+++ Source/Lib/ASM_NEON_I8MM/convolve_neon_i8mm.c
@@ -378,7 +378,7 @@ void svt_av1_convolve_y_sr_neon_i8mm(const uint8_t *src, int src_stride, uint8_t
 
     const int y_filter_taps = get_filter_tap(filter_params_y, subpel_y_qn);
 
-    if (y_filter_taps <= 6) {
+    if (y_filter_taps <= 4) {
         svt_av1_convolve_y_sr_neon(src,
                                    src_stride,
                                    dst,
@@ -393,12 +393,10 @@ void svt_av1_convolve_y_sr_neon_i8mm(const uint8_t *src, int src_stride, uint8_t
         return;
     }
 
-    const int vert_offset = y_filter_taps / 2 - 1;
-    src -= vert_offset * src_stride;
-
     const int16_t *y_filter_ptr = av1_get_interp_filter_subpel_kernel(*filter_params_y, subpel_y_qn & SUBPEL_MASK);
 
-    convolve_y_sr_8tap_neon_i8mm(src, src_stride, dst, dst_stride, w, h, y_filter_ptr);
+    // 6-tap or 8-tap.
+    convolve_y_sr_8tap_neon_i8mm(src - 3 * src_stride, src_stride, dst, dst_stride, w, h, y_filter_ptr);
 }
 static inline int16x8_t convolve8_8_2d_h(uint8x16_t samples, const int8x8_t filters, const uint8x16x3_t permute_tbl,
                                          const int32x4_t horiz_const) {
