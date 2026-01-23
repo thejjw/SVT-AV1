9485b93945a1dd87e93948ab18e594858bb89991
Hassene Tmar <htmar@meta.com>
2026-01-22 16:13:53 -0800

    Update README and Tune description

--- Source/API/EbSvtAv1Enc.h
+++ Source/API/EbSvtAv1Enc.h
@@ -641,7 +641,7 @@ typedef struct EbSvtAv1EncConfiguration {
 
     bool enable_overlays;
     /**
-     * @brief Tune for a particular metric; 0: VQ, 1: PSNR, 2: SSIM.
+     * @brief Tune for a particular metric; 0: VQ, 1: PSNR, 2: SSIM, 3: IQ (Image Quality), 4: MS-SSIM.
      *
      * Default is 1.
      */
