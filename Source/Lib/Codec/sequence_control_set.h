32cc37ba7a2c0535640386f0034d785a45da3a40
hguermaz <hassen.guermazi@intel.com>
2026-02-12 10:59:51 -0800

    Improve still-image coding efficiency
    Co-authored-by: Khalil Khemakhem <khalil.khemakhem@vlitt.com>
    Co-authored-by: Ahmed Kossentini <ahmed.kossentini@vlitt.com>
    Co-authored-by: Mohamed Amine Masmoudi <mohamed.masmoudi@vlitt.com>
    Co-authored-by: Taha Ben Romdhane <taha.benromdhane@vlitt.com>
    Co-authored-by: Iheb Trigui <iheb.trigui@vlitt.com>

--- Source/Lib/Codec/sequence_control_set.h
+++ Source/Lib/Codec/sequence_control_set.h
@@ -45,9 +45,14 @@ typedef struct QpBasedThScaling {
     bool nic_pruning_qp_based_th_scaling;
     bool pme_qp_based_th_scaling;
     bool txt_qp_based_th_scaling;
+#if !CLN_DR
     bool i_depth_removal_qp_based_th_scaling;
+#endif
     bool cap_max_size_qp_based_th_scaling;
     bool var_skip_sub_depth_qp_based_th_scaling;
+#if FTR_VLPD0
+    bool lpd0_qp_based_th_scaling;
+#endif
 } QpBasedThScaling;
 
 // Forward declaration for block geometry
