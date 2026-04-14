2d2fd4ffd30e646adb412882be9e5f6496bad923
Christopher Degawa <ccom@randomderp.com>
2026-04-14 16:33:48 -0700

    stylecheck: redirect stderr to stdout if in CI
    Due to the way that stderr can be unbuffered, it's possible for the
    heredoc printout to appear out of order, ruining the output.

--- test/stylecheck.sh
+++ test/stylecheck.sh
@@ -116,6 +116,10 @@ diff_output=$(
     git diff "$MERGE_BASE" -- "$@" | iconv -c -t UTF-8 | python3 "$REPO_DIR/test/clang-format-diff.py" -p1
 ) || true
 if [ -n "$diff_output" ]; then
+    if [ -n "$CI" ]; then
+        exec 2>&1
+    fi
+
     cat >&2 << 'FOE'
 clang-format check failed!
 Please run inside a posix compatible shell with git and amend or commit the
